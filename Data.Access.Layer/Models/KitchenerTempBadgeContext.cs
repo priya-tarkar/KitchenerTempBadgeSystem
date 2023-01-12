f2da4f82de1a64be9318b55e310e1d97e1fb95ec
priya-tarkar <priya.gla_cs19@gla.ac.in>
2023-01-12 14:30:30 +0530

    new changes added

--- Data.Access.Layer/Models/KitchenerTempBadgeContext.cs
+++ Data.Access.Layer/Models/KitchenerTempBadgeContext.cs
@@ -21,8 +21,9 @@ public partial class KitchenerTempBadgeContext : IdentityDbContext
     public virtual DbSet<Gaurd> Gaurds { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=CJHLT-1625\\SQLEXPRESS;Initial Catalog=KitchenerTempBadge;Encrypt=False;Integrated Security=True");
+    { }
+/* To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=CJHLT-1625\\SQLEXPRESS;Initial Catalog=KitchenerTempBadge;Encrypt=False;Integrated Security=True");*/
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
